4158c9bd734f8daf229c36ef551ebef6c091d1ca
sia <siavash.hassibi@gmail.com>
2022-01-29 18:37:28 -0800

    FINAL VERSION MT MANUAL

--- mtmanual.py
+++ mtmanual.py
@@ -16,11 +16,13 @@ def html():
     return x
 
 
-list = html()
+list = []
 #print(list)
 
 def type():
     global list
+    if len(list) == 0:
+        list = html()
     webdriver.ActionChains(driver).send_keys(list[0]).perform()
     list = list[1:]
 
@@ -30,56 +32,58 @@ p.press('tab')
 p.press('tab')
 p.keyUp('command')
 
-while True:
-    if keyboard.is_pressed('a'):
-        type()
-    if keyboard.is_pressed('b'):
-        type()
-    if keyboard.is_pressed('c'):
-        type()
-    if keyboard.is_pressed('d'):
-        type()
-    if keyboard.is_pressed('e'):
-        type()
-    if keyboard.is_pressed('f'):
-        type()
-    if keyboard.is_pressed('g'):
-        type()
-    if keyboard.is_pressed('h'):
-        type()
-    if keyboard.is_pressed('i'):
-        type()
-    if keyboard.is_pressed('j'):
-        type()
-    if keyboard.is_pressed('k'):
-        type()
-    if keyboard.is_pressed('l'):
-        type()
-    if keyboard.is_pressed('m'):
-        type()
-    if keyboard.is_pressed('n'):
-        type()
-    if keyboard.is_pressed('o'):
-        type()
-    if keyboard.is_pressed('p'):
-        type()
-    if keyboard.is_pressed('q'):
-        type()
-    if keyboard.is_pressed('r'):
-        type()
-    if keyboard.is_pressed('s'):
-        type()
-    if keyboard.is_pressed('t'):
-        type()
-    if keyboard.is_pressed('u'):
-        type()
-    if keyboard.is_pressed('v'):
-        type()
-    if keyboard.is_pressed('w'):
-        type()
-    if keyboard.is_pressed('x'):
-        type()
-    if keyboard.is_pressed('y'):
-        type()
-    if keyboard.is_pressed('z'):
-        type()
+def main():
+    while True:
+        if keyboard.is_pressed('a'):
+            type()
+        if keyboard.is_pressed('b'):
+            type()
+        if keyboard.is_pressed('c'):
+            type()
+        if keyboard.is_pressed('d'):
+            type()
+        if keyboard.is_pressed('e'):
+            type()
+        if keyboard.is_pressed('f'):
+            type()
+        if keyboard.is_pressed('g'):
+            type()
+        if keyboard.is_pressed('h'):
+            type()
+        if keyboard.is_pressed('i'):
+            type()
+        if keyboard.is_pressed('j'):
+            type()
+        if keyboard.is_pressed('k'):
+            type()
+        if keyboard.is_pressed('l'):
+            type()
+        if keyboard.is_pressed('m'):
+            type()
+        if keyboard.is_pressed('n'):
+            type()
+        if keyboard.is_pressed('o'):
+            type()
+        if keyboard.is_pressed('p'):
+            type()
+        if keyboard.is_pressed('q'):
+            type()
+        if keyboard.is_pressed('r'):
+            type()
+        if keyboard.is_pressed('s'):
+            type()
+        if keyboard.is_pressed('t'):
+            type()
+        if keyboard.is_pressed('u'):
+            type()
+        if keyboard.is_pressed('v'):
+            type()
+        if keyboard.is_pressed('w'):
+            type()
+        if keyboard.is_pressed('x'):
+            type()
+        if keyboard.is_pressed('y'):
+            type()
+        if keyboard.is_pressed('z'):
+            type()
+main()
